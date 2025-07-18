e09efe2cac4de998356888c47ca11087eee3b181
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-07-18 16:17:36 +0545

    Palindrome

--- Server.java
+++ Server.java
@@ -2,20 +2,33 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    public static void main(String[] arg) throws Exception{
-        ServerSocket ss=new ServerSocket(6000);
-        System.out.println("Server running at 6000");
+    public static void main(String[] args) throws Exception{
+        ServerSocket ss= new ServerSocket(7000);
+        System.out.println("Server running at 7000");
         Socket s= ss.accept();
-        System.out.println("client connected");
+        System.out.println("Client connected");
         DataInputStream dis= new DataInputStream(s.getInputStream());
         DataOutputStream dos= new DataOutputStream(s.getOutputStream());
-        int number=dis.readInt();
-        int result=number*number;
-        dos.writeInt(result);
+        String msg= dis.readUTF();
+        String check= isPalindrome(msg);
+        dos.writeUTF(check);
         dos.close();
         dis.close();
         s.close();
         ss.close();
+
     }
+    public static String isPalindrome(String str){
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--){
+            rev= rev+str.charAt(i);
+        }
+        if(rev.equalsIgnoreCase(str)){
+            return"Palindrome";
+
+        }
+        return"not Plaindrome";
+    }
+
     
 }
